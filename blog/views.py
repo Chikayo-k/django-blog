bb8f4458f8f8b8d7ce438e249d4ae91ec66ce0ac
Chikayo.K kam <chika000chika@gmail.com>
2024-04-15 09:13:55 +0000

    added comment CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
-from .models import Post
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
+from .forms import CommentForm
+
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -8,6 +12,31 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+
     
 def post_detail(request, slug):
     """
@@ -25,10 +54,32 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        print('Rerceived a POST request')
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request,messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
+    print('About to render template')
 
     return render(
         request,
         "blog/post_detail.html",
         {"post": post,
-         'coder':'Chika Rudge'},
+         'comments':comments,
+         "comments": comments,
+         "comment_count": comment_count,
+         "comment_form": comment_form,
+        },
     )
